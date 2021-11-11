b5b9330909b5f7159cbd8ac1a07975a428b624a7
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-11-11 16:45:17 +0000

    dependency injection done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: any = [
+    {
+      id: 0,
+      name: 'Maruis',
+    },
+    {
+      id: 1,
+      name: 'Ibrahim',
+    },
+    {
+      id: 2,
+      name: 'Muhammad',
+    },
+  ];
+
+  getUsers(): any {
+    return this.users;
+  }
+
+  getUserById(userId: number): any {
+    return this.users.find((user) => user.id === userId);
+  }
+}

54c2d954f53708bc1e17dbcae024d8e3225f69f5
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-11-12 04:05:16 +0000

    swagger Api decorators done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,24 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getUsers(): User[] {
     return this.userService.getUsers();
   }
+  @ApiOkResponse({ type: User })
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.userService.getUserById(Number(id));
   }
-
+  @ApiCreatedResponse({ type: User })
   @Post()
   CreateUser(@Body() body: CreateUserDto): any {
     return this.userService.createUser(body);
